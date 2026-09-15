10879f67072355adee1261d4304e2a084e255429
vivek-singh76 <vivek.kumarsingh.cs.@2023mitmeerut.ac.in>
2026-09-15 07:41:41 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB successfully");
     app.listen(PORT, () => {
